03e3b4ae3eb6a224f8a00299e69bbbbb1f7b3dc6
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-04 15:32:55 +0530

    created Daniel as Employee Object

--- src/main/java/com/niit/jdp/EmployeeImpl.java
+++ src/main/java/com/niit/jdp/EmployeeImpl.java
@@ -2,6 +2,10 @@ package com.niit.jdp;
 
 public class EmployeeImpl {
     public static void main(String[] args) {
-
+        Employee Daniel = new Employee("Daniel",
+                130,
+                32,
+                "124, Bridgewater, Eville",
+                3000.0);
     }
 }
