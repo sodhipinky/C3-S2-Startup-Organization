84713577d69ce5f6caa34d27fb6c98def7d6ef9f
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-04 15:28:32 +0530

    created a parameterized constructor for Employee class

--- src/main/java/com/niit/jdp/Employee.java
+++ src/main/java/com/niit/jdp/Employee.java
@@ -6,4 +6,12 @@ public class Employee {
     int age;
     String address;
     double salary;
+
+    public Employee(String name, long code, int age, String address, double salary) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.address = address;
+        this.salary = salary;
+    }
 }
