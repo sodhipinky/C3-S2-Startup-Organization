8079ec24a48a2aafe7d1ea2a8a8666ac1d040d97
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-04 15:35:50 +0530

    called the methods with the Object

--- src/main/java/com/niit/jdp/EmployeeImpl.java
+++ src/main/java/com/niit/jdp/EmployeeImpl.java
@@ -2,10 +2,14 @@ package com.niit.jdp;
 
 public class EmployeeImpl {
     public static void main(String[] args) {
-        Employee Daniel = new Employee("Daniel",
+        Employee daniel = new Employee("Daniel",
                 130,
                 32,
                 "124, Bridgewater, Eville",
                 3000.0);
+        double annualSalary = daniel.calculateAnnualSalary();
+        System.out.println("Annual Salary Of Daniel = $" + annualSalary);
+        double salaryAfterPayRaise = daniel.calculateSalaryAfterPayRaise(20);
+        System.out.println("Salary Of Daniel After Pay Raise = " + salaryAfterPayRaise);
     }
 }
